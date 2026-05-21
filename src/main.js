9a6a47dba88aea1b87e3bf7217c9a605a2cc58ab
theskilledbanana <s1227701@terrace.qld.edu.au>
2026-05-22 08:06:03 +1000

    feat(data): update Golf Orbit thumbnail URL
    Replace local thumbnail asset with a remote CDN link to ensure proper image loading.

--- src/main.js
+++ src/main.js
@@ -53,7 +53,7 @@ const allEntries = [
     "id": "golf-orbit",
     "title": "Golf Orbit",
     "iframeUrl": "https://lesson126.github.io/lesson83/lesson-2123",
-    "thumbnail": "./golf-orbit.png",
+    "thumbnail": "https://imgs.crazygames.com/golf-orbit_16x9/20240620070701/golf-orbit_16x9-cover?metadata=none&quality=60&height=5527",
     "categories": ["Sports", "Skill", "Arcade"],
     "description": "Launch your golf ball into the stratosphere! Master the perfect swing to send your ball orbiting through space in this addictive timing-based arcade game."
   }
