f100f5ed2a44f9c5e4ef774fdafec56f9f13e66b
theskilledbanana <s1227701@terrace.qld.edu.au>
2026-05-21 21:00:35 +1000

    feat: Use local thumbnail for Golf Orbit
    Replaces the remote thumbnail URL for the "Golf Orbit" game with a locally hosted image. This change improves performance and reliability by reducing external dependencies.

--- src/main.js
+++ src/main.js
@@ -53,7 +53,7 @@ const allEntries = [
     "id": "golf-orbit",
     "title": "Golf Orbit",
     "iframeUrl": "https://lesson126.github.io/lesson83/lesson-2123",
-    "thumbnail": "https://img.gamedistribution.com/02ff01e6e9f7451ba28d889a2a55eec9-512x512.jpg",
+    "thumbnail": "/golf-orbit.png",
     "categories": ["Sports", "Skill", "Arcade"],
     "description": "Launch your golf ball into the stratosphere! Master the perfect swing to send your ball orbiting through space in this addictive timing-based arcade game."
   }
